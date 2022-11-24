9edb87b0924f104a9d7fb1740b392fd3c8df03ba
jakegoodwin <jakegoodwin@tutanota.com>
2022-11-23 19:40:17 -0800

    added example for the 5 step proccess

--- js/main.js
+++ js/main.js
@@ -4,6 +4,11 @@
  * Description: The main file for the projects JS.
  */
 
+//SOURCES:
+// MDN web docs https://developer.mozilla.org/en-US/docs/Web
+// Tutorials Point https://www.tutorialspoint.com/webgl/index.html
+// 
+
 //Make sure we wait for the page to load.
 
 //################################
@@ -12,6 +17,18 @@
 
 
 
+class Vertex {
+    constructor(x, y, z, r=0.0, g=0.0, b=0.0) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.r = r; //Red value
+        this.g = g; //Green value
+        this.b = b; //Blue value
+        this.s = s; //Scalar value 
+    }
+}
+
 
 
 
@@ -19,10 +36,14 @@ function main(){
     console.log("main()\n");
 
     //get the context.
-    var gl = getRenderingContext();
+    var [gl, canvas] = getRenderingContext();
 
     //clearing with solid color.
-    clear_with_color(gl);
+    //clear_with_color(gl);
+
+    //var dot = Vertex.new(0, 0, 0);
+    //draw_dot(gl, dot);
+    tringle_example(gl, canvas);   
 
     return;
 }
@@ -60,7 +81,7 @@ function getRenderingContext() {
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT);
 
-    return gl;
+    return [gl, canvas];
 }
 
 function clear_with_color(gl) {
@@ -70,13 +91,92 @@ function clear_with_color(gl) {
 }
 
 
-function draw_dot() {
-
+function draw_dot(gl, v) {
+    console.log("draw_dot()");
+    return;
 }
 
 
 
+function tringle_example(gl, canvas) {
+    //define our data and store into buffer obj
+    var vertices = [-0.5, 0.5, -0.5, -0.5, 0.0, -0.5,];
 
+    var vertex_buffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertex_buffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+    gl.bindBuffer(gl.ARRAY_BUFFER, null);
+
+
+    //Create and compile shader pgrms
+    // Vertex shader source code
+    var vertCode =
+        'attribute vec2 coordinates;' + 
+        'void main(void) {' + ' gl_Position = vec4(coordinates,0.0, 1.0);' + '}';
+
+    var vertShader = gl.createShader(gl.VERTEX_SHADER);
+    gl.shaderSource(vertShader, vertCode);
+    gl.compileShader(vertShader);
+
+    var fragCode = 'void main(void) {' + 'gl_FragColor = vec4(0.0, 0.0, 0.0, 0.1);' + '}';
+
+     // Create fragment shader object
+     var fragShader = gl.createShader(gl.FRAGMENT_SHADER);
+
+     // Attach fragment shader source code
+     gl.shaderSource(fragShader, fragCode);
+
+     // Compile the fragment shader
+     gl.compileShader(fragShader);
+
+     // Create a shader program object to store combined shader program
+     var shaderProgram = gl.createProgram();
+
+     // Attach a vertex shader
+     gl.attachShader(shaderProgram, vertShader);
+
+     // Attach a fragment shader
+     gl.attachShader(shaderProgram, fragShader);
+
+     // Link both programs
+     gl.linkProgram(shaderProgram);
+
+     // Use the combined shader program object
+     gl.useProgram(shaderProgram);
+
+     /* Step 4: Associate the shader programs to buffer objects */
+
+     //Bind vertex buffer object
+     gl.bindBuffer(gl.ARRAY_BUFFER, vertex_buffer);
+
+     //Get the attribute location
+     var coord = gl.getAttribLocation(shaderProgram, "coordinates");
+
+     //point an attribute to the currently bound VBO
+     gl.vertexAttribPointer(coord, 2, gl.FLOAT, false, 0, 0);
+
+     //Enable the attribute
+     gl.enableVertexAttribArray(coord);
+
+     /* Step5: Drawing the required object (triangle) */
+
+     // Clear the canvas
+     gl.clearColor(0.5, 0.5, 0.5, 0.9);
+
+     // Enable the depth test
+     gl.enable(gl.DEPTH_TEST);
+
+     // Clear the color buffer bit
+     gl.clear(gl.COLOR_BUFFER_BIT);
+
+     // Set the view port
+     gl.viewport(0,0,canvas.width,canvas.height);
+
+     // Draw the triangle
+     gl.drawArrays(gl.LINE_LOOP, 0, 3);
+
+
+}
 
 
 
