642de2a5f76a9f20268631f3666ead6c753bb96b
jakegoodwin <jakegoodwin@tutanota.com>
2022-11-23 18:58:35 -0800

    updated js using webgl to color the display red.

--- js/main.js
+++ js/main.js
@@ -14,18 +14,15 @@
 
 
 
-main();
 
 function main(){
     console.log("main()\n");
 
-    const gl = canvas.getContext("webgl") 
-        || canvas.getContext("experimental-webgl");
-
-
+    //get the context.
+    var gl = getRenderingContext();
 
     //clearing with solid color.
-    clear_with_color();
+    clear_with_color(gl);
 
     return;
 }
@@ -44,9 +41,31 @@ function main(){
 //################################
 
 
+function getRenderingContext() {
+    console.log("getRenderingContext()\n");
+
+    const canvas = document.querySelector("canvas");
+    canvas.width = canvas.clientWidth;
+    canvas.height = canvas.clientHeight;
+    const gl = canvas.getContext("webgl")
+        || canvas.getContext("experimental-webgl");
+    if (!gl) {
+        const paragraph = document.querySelector("p");
+        paragraph.innerHTML = "Failed to get WebGL context."
+        + "Your browser or device may not support WebGL.";
+        return null;
+    }
+
+    gl.viewport(0, 0, gl.drawingBufferWidth, gl.drawingBufferHeight);
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    return gl;
+}
+
 function clear_with_color(gl) {
     gl.viewport(0, 0, gl.drawingBufferWidth, gl.drawingBufferHeight);
-    gl.clearColor(0.0, 0.5, 0.0, 1.0);
+    gl.clearColor(1.0, 0.0, 0.0, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT);
 }
 
