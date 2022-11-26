d16169c5910b306a7af423542aec80ed82316158
jakegoodwin <jakegoodwin@tutanota.com>
2022-11-25 17:30:40 -0800

    updated with CRUD functions, not fully defined yet.

--- app_server/src/main.rs
+++ app_server/src/main.rs
@@ -4,6 +4,8 @@ extern crate rocket;
 use rocket::form::Form;
 //use rocket::http::RawStr;
 
+use std::process::Command;
+
 /*
  * #################################
  * DATA STRUCTS
@@ -27,6 +29,33 @@ struct Feedback {
 
 //Create ROUTES
 
+/*
+ * Input: New feedback.
+ * Output: feedback to clients.
+ * Desc: Sends clients ping to update with created feedback.
+ */
+fn create_feedback() -> &'static str {
+    "Ok"
+}
+
+/*
+ * Input: Data for user
+ * Output: user data into file/database
+ * Desc: Responds with results of creation.
+ */
+fn create_user() -> &'static str {
+    "Ok"
+}
+
+/*
+ * Input: Data for project
+ * Output: project data into file/database
+ * Desc: Creates a new instance for project
+ */
+fn create_project() -> &'static str {
+    "Ok"
+}
+
 //Read ROUTES
 
 /*
@@ -44,7 +73,7 @@ fn index() -> &'static str {
  * Output: json
  * Desc: Reads the data from a csv file into json and sends it.
  */
-#[get("/read/data_csv/<filename>")]
+#[get("/data_csv/<filename>")]
 fn data_csv(filename: &str) -> &'static str {
     "Nothing for now."
 }
@@ -54,9 +83,16 @@ fn data_csv(filename: &str) -> &'static str {
  * Output: json.
  * Desc: Generates current git log / status.
  */
-#[get("/read/git_log")]
-fn git_log() -> &'static str {
-    "GIT LOG HERE"
+#[get("/git_log")]
+fn git_log() -> String {
+    //Call the system git command.
+    let output = Command::new("git")
+        .arg("log")
+        .output()
+        .expect("Failed to execute process.");
+    //Add error handling
+    let txt = String::from_utf8(output.stdout).unwrap();
+    txt
 }
 
 /*
@@ -64,9 +100,16 @@ fn git_log() -> &'static str {
  * Output: json.
  * Description: Sends out the newest X amount of feedback.
  */
-#[get("/read/feedback")]
-fn read_feedback() -> &'static str {
-    "Feedback here."
+#[get("/feedback")]
+fn read_feedback() -> String {
+    //Call unix command to grab the feedback text.
+    let output = Command::new("cat")
+        .arg("feedback.txt")
+        .output()
+        .expect("Failed to execute process.");
+    //Add error handling
+    let txt = String::from_utf8(output.stdout).unwrap();
+    txt
 }
 
 /*
@@ -74,7 +117,7 @@ fn read_feedback() -> &'static str {
  * Output: Code as data
  * Description: Gets the requested revision of the webgl code.
  */
-#[get("/read/webgl_code/<version>")]
+#[get("/webgl_code/<version>")]
 fn read_webgl_code(version: u64) -> &'static str {
     "webgl code"
 }
@@ -86,8 +129,9 @@ fn read_webgl_code(version: u64) -> &'static str {
  * Output: Ack.
  * Description: Updates the recent feedback on the server.
  */
-#[post("/update/feedback", data = "<new_feedback>")]
+#[post("/feedback", data = "<new_feedback>")]
 fn update_feedback(new_feedback: Form<Feedback>) -> &'static str {
+    //Push data into our global memory/then append it too the file.
     "feedback!"
 }
 
@@ -98,8 +142,9 @@ fn update_feedback(new_feedback: Form<Feedback>) -> &'static str {
  * Output: ack
  * Description: Deletes requested data.
  */
-#[get("/delete/feedback")]
+#[get("/feedback")]
 fn del_feedback() -> &'static str {
+    //Probably want a user check here.
     "Ok"
 }
 
@@ -112,4 +157,5 @@ fn rocket() -> _ {
             routes![data_csv, git_log, read_feedback, read_webgl_code],
         )
         .mount("/delete", routes![del_feedback])
+        .mount("/create", routes![create_feedback])
 }
